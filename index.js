71c883a92b3cb491725bbed8ac26fa3e2fc45c87
lopez-mgu <lopez.mgu@gmail.com>
2022-03-24 23:39:18 -0700

    second commit by MG

---
+++ index.js
@@ -0,0 +1,22 @@
+function getNumNat(){
+    let turistas=[];
+    for (let i = 0; i < 4; i++) {
+
+        turistas[i]=
+            {nombre: document.getElementById('input-nombre0'+(i+1)).value,
+            edad: document.getElementById('input-edad0'+(i+1)).value,
+            nacionalidad: document.getElementById('dropdown0'+(i+1)).value,}
+    }
+    let Mexico=turistas.filter(function (item){return item.nacionalidad==='Mexicano'});
+    console.log(`Mexicanos: ${Mexico.length}`);
+
+    let Peru=turistas.filter(function (item){return item.nacionalidad==='Peruano'});
+    console.log(`Peruanos: ${Peru.length}`);
+
+    let Argentina=turistas.filter(function (item){return item.nacionalidad==='Argentino'});
+    console.log(`Argentinos: ${Argentina.length}`);
+
+    let Venezuela=turistas.filter(function (item){return item.nacionalidad==='Venezolano'});
+    console.log(`Venezolanos: ${Venezuela.length}`);
+}
+
